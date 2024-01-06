5a7f876a9e468e9d248cb18209f2dc5143bd3112
shmuelko <shmuel777@gmail.com>
2024-01-06 17:47:06 +0200

    finish

--- src/tracks_on_tracks_on_tracks.clj
+++ src/tracks_on_tracks_on_tracks.clj
@@ -1,28 +1,32 @@
 (ns tracks-on-tracks-on-tracks)
 
 (defn new-list
-  "Creates an empty list of languages to practice."
-  []
-  )
+      "Creates an empty list of languages to practice."
+      []
+      '())
 
 (defn add-language
   "Adds a language to the list."
   [lang-list lang]
+      (conj lang-list lang)
   )
 
 (defn first-language
   "Returns the first language on the list."
   [lang-list]
+      (first lang-list)
   )
 
 (defn remove-language
   "Removes the first language added to the list."
   [lang-list]
+      (drop 1 lang-list)
   )
 
 (defn count-languages
   "Returns the total number of languages on the list."
   [lang-list]
+      (count lang-list)
   )
 
 (defn learning-list
@@ -30,4 +34,11 @@
   Java and JavaScript, then finally returns a count of the total number
   of languages."
   []
+      (-> (new-list)
+          (add-language "Clojure")
+          (add-language "Lisp")
+          (remove-language)
+          (add-language "Java")
+          (add-language "JavaScript")
+          (count-languages))
   )
